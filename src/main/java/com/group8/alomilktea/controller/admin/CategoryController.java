bae165e027ecb5a893a829d5bfbaa4df698dc013
DangDuyy <duyprovn987@gmail.com>
2024-11-29 16:31:08 +0700

    show list category thành công

--- src/main/java/com/group8/alomilktea/controller/admin/CategoryController.java
+++ src/main/java/com/group8/alomilktea/controller/admin/CategoryController.java
@@ -2,6 +2,7 @@ package com.group8.alomilktea.controller.admin;
 
 import com.group8.alomilktea.entity.Category;
 import com.group8.alomilktea.service.ICategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -13,14 +14,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/admin/category")
 public class CategoryController {
 
+    @Autowired
     private ICategoryService categoryService;
 
     @RequestMapping("/list")
-    public String list(ModelMap model, @RequestParam(name="pageNo", defaultValue = "0") Integer pageNo) {
+    public String showCategoryList(ModelMap model, @RequestParam(name="pageNo", defaultValue = "0") Integer pageNo) {
 
         Page<Category> categoryPage = categoryService.getAll(pageNo);
 
+        model.addAttribute("categories", categoryPage);
+        model.addAttribute("currentPage", pageNo);
+        model.addAttribute("totalPages", categoryPage.getTotalPages());
+
+
         return "admin/categories/apps-ecommerce-category-list";
+    }
+
+    @RequestMapping("/add")
+    public String addCategory(ModelMap model){
+        return "admin/categories/apps-ecommerce-category-create";
     }
 
 }
