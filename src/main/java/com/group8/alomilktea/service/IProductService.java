d71e1a0d258ec000102ba543d10c6d4fcfc50cb9
NhanSamLe <ledaonhansam001@gmail.com>
2024-11-30 20:17:53 +0700

    updatee

--- src/main/java/com/group8/alomilktea/service/IProductService.java
+++ src/main/java/com/group8/alomilktea/service/IProductService.java
@@ -1,5 +1,33 @@
 package com.group8.alomilktea.service;
 
+import com.group8.alomilktea.entity.Product;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IProductService {
 
+    <S extends Product> Optional<S> findOne(Example<S> example);
+
+    List<Product> findAll(Sort sort);
+
+    Optional<Product> findById(Integer integer);
+
+    void deleteById(Integer integer);
+
+    void deleteAllById(Iterable<? extends Integer> integers);
+
+    long count();
+
+    void deleteAll();
+
+    void delete(Product entity);
+
+    List<Product> findAll();
+
+    List<Product> findAllById(Iterable<Integer> integers);
+
+    <S extends Product> List<S> findAll(Example<S> example);
 }
--- src/main/java/com/group8/alomilktea/service/impl/ProductService.java
+++ src/main/java/com/group8/alomilktea/service/impl/ProductService.java
@@ -15,49 +15,60 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
-public class IProductService implements com.group8.alomilktea.service.IProductService {
+public class ProductService implements com.group8.alomilktea.service.IProductService {
     ProductRepository productRepository;
 
+    @Override
     public <S extends Product> Optional<S> findOne(Example<S> example) {
         return productRepository.findOne(example);
     }
 
+    @Override
     public List<Product> findAll(Sort sort) {
         return productRepository.findAll(sort);
     }
 
+    @Override
     public Optional<Product> findById(Integer integer) {
         return productRepository.findById(integer);
     }
 
+    @Override
     public void deleteById(Integer integer) {
         productRepository.deleteById(integer);
     }
 
+    @Override
     public void deleteAllById(Iterable<? extends Integer> integers) {
         productRepository.deleteAllById(integers);
     }
 
+    @Override
     public long count() {
         return productRepository.count();
     }
 
+    @Override
     public void deleteAll() {
         productRepository.deleteAll();
     }
 
+    @Override
     public void delete(Product entity) {
         productRepository.delete(entity);
     }
 
+    @Override
     public List<Product> findAll() {
         return productRepository.findAll();
     }
 
+    @Override
     public List<Product> findAllById(Iterable<Integer> integers) {
         return productRepository.findAllById(integers);
     }
 
+    @Override
     public <S extends Product> List<S> findAll(Example<S> example) {
         return productRepository.findAll(example);
     }
